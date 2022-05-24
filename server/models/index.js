94b559188ecc796fda0b6efc03836e8261cd2f77
Owen Foreman <owen4man@gmail.com>
2022-05-24 10:19:26 -0400

    folders

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const User = require('./user');
+const Game = require('./game');
 
-module.exports = { User, Thought };
+module.exports = { User, Game };
