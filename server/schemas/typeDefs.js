70288eee3d3f95abcb0a8543a8c815b61aa31709
AugustusMadden <gusmadden@gmail.com>
2022-05-24 12:50:40 -0400

    typeDef Layout

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,19 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        password: String
+    }
+
+    type Game {}
+
+    type GameList {}
+
+    type Query {}
+
+    type Mutation {}
 `;
 
 module.exports = typeDefs;
