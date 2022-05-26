84409c3e69455947bc88a6fd9f83eb3db05b29d2
AugustusMadden <gusmadden@gmail.com>
2022-05-26 16:10:52 -0400

    Deploy update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/VDB-content',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/IV-database',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
