b4dd1bae6229c752733e1052b81c97c0a90b36da
Kaleb Sherrill <kaleb1027@gmail.com>
2022-05-26 13:34:40 -0400

    finished queries.js for accessible data

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -14,13 +14,52 @@ query users {
     users {
       _id
       username
-      password
     }
   }
 `;
 
-export const QUERY_GAME = gql
+export const QUERY_GAMES = gql`
+query Games {
+    games {
+      name
+      _id
+      slug
+    }
+  }
+`
 
+export const QUERY_GAME = gql`
+query Game($gameId: ID) {
+    game(gameId: $gameId) {
+      _id
+      slug
+      name
+      released
+      image
+    }
+  }`
+
+export const QUERY_GAMELISTS = gql`
+query GameLists($userId: ID) {
+    gameLists(userId: $userId) {
+    _id
+    listName
+    games {
+      name
+    }  
+    }
+  }`
+
+export const QUERY_GAMELIST = gql`
+query GameLists($listId: ID!) {
+    gameList(listId: $listId) {
+    listName
+    games {
+      name
+      slug
+    }  
+    }
+  }`
 
 // users: [User]
 //         user(username: String!): User
